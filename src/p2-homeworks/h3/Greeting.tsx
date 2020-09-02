955a0733e8ffd0b254ab3d473f68655d94845a1b
Sergey-Lang <63201050+Sergey-lang@users.noreply.github.com>
2020-09-02 07:47:24 +0300

    h3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,29 @@
-import React from "react";
-import s from "./Greeting.module.css";
+import React from 'react';
+import s from './Greeting.module.css';
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: any// need to fix any
+    addUser: () => void// need to fix any
     error: any // need to fix any
-    totalUsers: any // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
-) => {
-    const inputClass = s.error; // need to fix with (?:)
+    {name, setNameCallback, addUser, error, totalUsers}) => {
+
+    const inputClass = error !== '' ? s.error : s.someClass; // need to fix with (?:)
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.wrapper}>
+            <div className={s.group}>
+                <input value={name} onChange={setNameCallback} className={inputClass}/>
+                <span className={s.bar}></span>
+                <span className={s.errorBlock}>{error}</span>
+                <button onClick={addUser} className={s.modern}>add</button>
+                <span className={s.userCount}>Users count: {totalUsers}</span>
+            </div>
         </div>
     );
 }
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,38 +1,47 @@
-import React, {useState} from "react";
-import Greeting from "./Greeting";
+import React, {useState, ChangeEvent,} from 'react';
+import Greeting from './Greeting';
+import {UserType} from './HW3';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType>
+    addUserCallback: (name: string) => void
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
+const GreetingContainer: React.FC<GreetingContainerPropsType> =
+    ({users, addUserCallback}) => {
 
-// более современный и удобный для про :)
-// уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>(""); // need to fix any
-    const [error, setError] = useState<any>(""); // need to fix any
+        const [name, setName] = useState('');
+        const [error, setError] = useState('');
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName(""); // need to fix
-    };
-    const addUser = () => {
-        alert(`Hello  !`); // need to fix
-    };
+        const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+            setError('')
+            setName(e.currentTarget.value);
+        };
 
-    const totalUsers = 0; // need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    );
-}
+        const addUser = () => {
+            if (name !== '') {
+                addUserCallback(name)
+                alert('Hello ' + name)
+                
+            } else {
+                let err = 'Name is not defined'
+                setError(err)
+            }
+
+        };
+
+        const totalUsers = users.length;
+
+        return (
+            <Greeting
+                name={name}
+                setNameCallback={setNameCallback}
+                addUser={addUser}
+                error={error}
+                totalUsers={totalUsers}
+            />
+        );
+    }
 
 export default GreetingContainer;
