cb0616dd676f4b4d09d900f6cbb28f47207ac821
Sergey-lang <sergei.kuharyonok@yandex.by>
2020-09-23 20:38:19 +0300

    Updates

---
+++ precache-manifest.a0e126279add299bcfea918a35abbdd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98fd83e6c265d11c77efa5d2d46a7ee",
+    "url": "/Simple-React-HW/index.html"
+  },
+  {
+    "revision": "0e7ef8804f1b0bf56ba3",
+    "url": "/Simple-React-HW/static/css/main.d4c045a1.chunk.css"
+  },
+  {
+    "revision": "c0922306f186b90417a3",
+    "url": "/Simple-React-HW/static/js/2.6682f795.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-React-HW/static/js/2.6682f795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7ef8804f1b0bf56ba3",
+    "url": "/Simple-React-HW/static/js/main.e9df20c5.chunk.js"
+  },
+  {
+    "revision": "6f8bdabcf283a54fb81b",
+    "url": "/Simple-React-HW/static/js/runtime-main.53ea6b7a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Simple-React-HW/precache-manifest.754e0d22923a52a2e0ded96ada3c1793.js"
+  "/Simple-React-HW/precache-manifest.a0e126279add299bcfea918a35abbdd7.js"
 );
 
 self.addEventListener('message', (event) => {
