fd3f9ec12990a4ab709b96f0efa5e10405eabc6a
Sergey-lang <sergei.kuharyonok@yandex.by>
2020-10-01 16:17:27 +0300

    homework 7 completed 100% completed

--- src/p2-homeworks/h5/pages/Junior/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior/Junior.tsx
@@ -4,11 +4,12 @@ import HW1 from "../../../h1/HW1";
 import HW2 from "../../../h2/HW2";
 import HW3 from "../../../h3/HW3";
 import HW4 from "../../../h4/HW4";
+import HW7 from '../../../h7/HW7';
 
 function Junior() {
     return (
         <div className={s.prejunplus_wrapper}>
-            <h2>This will be something interesting...JUNIOR</h2>
+            <HW7/>
         </div>
     );
 }
---
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,34 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react';
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        name,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    const mappedOptions: any[] = options ? options.map((o, i) => {
+            return <option key={name + "-" + i} value={o}>{o}</option>
+        })
+        : [] // map options with key
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChangeOption && onChangeOption(e.target.value)
+    }
+
+    return (
+        <select onChange={onChangeCallback} {...restProps} >
+            {mappedOptions}
+        </select>
+    );
+}
+
+export default SuperSelect;
